e2b5242a20df694f4145b2021d92ad4e2ff882e1
Dicklesworthstone <jeff141421@gmail.com>
2025-11-25 23:23:56 +0000

    Fix Homebrew checksum for v1.1.3

--- HomebrewFormula/srps.rb
+++ HomebrewFormula/srps.rb
@@ -2,7 +2,7 @@ class Srps < Formula
   desc "System Resource Protection Script (Ananicy-cpp + helpers)"
   homepage "https://github.com/Dicklesworthstone/system_resource_protection_script"
   url "https://github.com/Dicklesworthstone/system_resource_protection_script/archive/refs/tags/v1.1.3.tar.gz"
-  sha256 "710324e07fef5df171687eb4025b64de632eaedbef541c630ad95678be2d1403"
+  sha256 "7310bd4bd13705d49c1cbedfad95cccd39cf5a2ab0d88b07db80ceb7b9fcf048"
   license "MIT"
 
   head "https://github.com/Dicklesworthstone/system_resource_protection_script.git", branch: "main"
